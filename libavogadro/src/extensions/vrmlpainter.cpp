c79523131dcc0040b5d62710463ba4e00fb37cd3
Ethan <etp12@pitt.edu>
2016-03-28 19:09:11 -0400

    multi-order bonds appear to be drawing correctly

--- libavogadro/src/extensions/vrmlpainter.cpp
+++ libavogadro/src/extensions/vrmlpainter.cpp
@@ -164,6 +164,42 @@ namespace Avogadro
 	{
 		if (order == 1)
 			drawCylinder(end1, end2, radius);
+
+		else {
+			//taken from povpainter.cpp
+			// Find the bond axis
+			Vector3d axis = end2 - end1;
+			double axisNorm = axis.norm();
+			if (axisNorm < 1.0e-5) return;
+			Vector3d axisNormalized = axis / axisNorm;
+
+			// Use the plane normal vector for the molecule to draw multicylinders along
+			Vector3d ortho1 = axisNormalized.cross(d->planeNormalVector);
+			double ortho1Norm = ortho1.norm();
+			if (ortho1Norm > 0.001) ortho1 /= ortho1Norm;
+			else ortho1 = axisNormalized.unitOrthogonal();
+			// This number seems to work well for drawing the multiCylinder inside
+			ortho1 *= radius*1.5;
+			Vector3d ortho2 = axisNormalized.cross(ortho1);
+			// Use an angle offset of zero for double bonds, 90 for triple and 22.5 for higher order
+			double angleOffset = 0.0;
+			if (order >= 3) {
+				if (order == 3)
+					angleOffset = 90.0;
+				else
+					angleOffset = 22.5;
+			}
+			// Actually draw the cylinders
+			for (int i = 0; i < order; ++i) {
+				double alpha = angleOffset / 180.0 * M_PI + 2.0 * M_PI * i / order;
+				Vector3d displacement = cos(alpha) * ortho1 + sin(alpha) * ortho2;
+				Vector3d displacedEnd1 = end1 + displacement;
+				Vector3d displacedEnd2 = end2 + displacement;
+
+				drawCylinder(displacedEnd1, displacedEnd2, radius);
+
+			}
+		}
 	}
 
 	void VRMLPainter::drawShadedSector(const Eigen::Vector3d &,
